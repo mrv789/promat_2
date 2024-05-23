80814c75d81f1ec51a618a4b554aa6fcfc9c191b
mrv789 <92297797+mrv789@users.noreply.github.com>
2024-05-23 19:57:43 +0300

    Add files via upload

---
+++ task3/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задайте произвольный массив. 
+Выведете его элементы, начиная с конца. 
+Использовать рекурсию, не использовать циклы.
+*/
+
+void RevMas(int[] array, int i = 0)
+{
+    if (i < array.Length)
+    {
+        RevMas(array, i + 1);
+        Console.Write(array[i] + " ");
+    }
+}
+int[] array = { 1, 2, 5, 0, 10, 34 };
+RevMas(array);
