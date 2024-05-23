98088b709009dfcc9e23f80ac9cf24ae05588d31
mrv789 <92297797+mrv789@users.noreply.github.com>
2024-05-23 19:51:54 +0300

    Add files via upload

---
+++ task1/Program.cs
@@ -0,0 +1,26 @@
+﻿ // Задача 1: Задайте значения M и N. 
+ // Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+ // Использовать рекурсию, не использовать циклы.
+
+
+
+
+void NumberOfToN(int m, int n)
+{
+    if (m > n)
+    {
+        NumberOfToN(m - 1, n);
+        System.Console.Write(m + " ");
+    }
+    else if (n > m)
+    {
+        NumberOfToN(m, n-1);
+        System.Console.Write(n + " ");
+    }
+    else 
+        System.Console.Write(m + " ");
+}
+
+// NumberOfToN (1, 5);
+
+NumberOfToN (4, 8);
